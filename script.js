f9a9d5f1589e3e649c95aba0ed75e242a99b01bd
AmjadHayat28 <amjadhayat946010@gmail.com>
2024-10-29 04:08:25 -0700

    first upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let noBtn = document.querySelector(".noBtn");
+    let yesBtn = document.querySelector(".yesBtn");
+    let heading = document.querySelector("h1");
+    noBtn.addEventListener('mouseenter', () => {
+        // Generate random positions within the viewport dimensions
+        const randomX = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+        const randomY = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+
+        // Set the button's new position
+        noBtn.style.position = 'absolute';
+        noBtn.style.left = `${randomX}px`;
+        noBtn.style.top = `${randomY}px`;
+    });
+
+    // Corrected the event listener for yesBtn
+    yesBtn.addEventListener("click", () => {
+        heading.innerHTML = "I knew it tema  🤭 <br/> now unblock me from whatsapp"
+        yesBtn.style.display="none";
+        noBtn.style.display="none";
+    });
+});
